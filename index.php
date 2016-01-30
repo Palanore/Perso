fe9181d93ca3bc3d8b766173c1690f88a61c996b
Palanore <babar1088@laposte.net>
2016-01-30 09:37:04 +0100

    modif

--- index.php
+++ index.php
@@ -1,13 +1,7 @@
 <?php
-<<<<<<< HEAD
 	echo "Hello Mini world";
-?>
-=======
+
 	echo "Hello World";
 
 	echo "Au revoir";
 ?>
-<<<<<<< HEAD
->>>>>>> Palanore/master
-=======
->>>>>>> refs/remotes/origin/master
